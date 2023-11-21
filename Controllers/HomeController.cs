07f285146ff48f798d450d4d1e8af1b83756e52c
inTheOctagon <mehmetutkununoyunlari@gmail.com>
2023-11-21 11:32:42 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModularAjaxPractice.Models;
+using System.Diagnostics;
+
+namespace ModularAjaxPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<Data> datas = new List<Data>();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            if (!datas.Any())
+            {
+                datas.Add(new Data(){ Id = 1, Context = "Context 1" });
+                datas.Add(new Data(){ Id = 2, Context = "Context 2" });
+                datas.Add(new Data(){ Id = 3, Context = "Context 3" });
+                datas.Add(new Data(){ Id = 4, Context = "Context 4" });
+                datas.Add(new Data(){ Id = 5, Context = "Context 5" });
+                datas.Add(new Data(){ Id = 6, Context = "Context 6" });
+                datas.Add(new Data(){ Id = 7, Context = "Context 7" });
+                datas.Add(new Data(){ Id = 8, Context = "Context 8" });
+                datas.Add(new Data(){ Id = 9, Context = "Context 9" });
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View(datas);
+        }
+
+        public IActionResult GetData(int id)
+        {
+            var data = datas.Find(x => x.Id == id);
+
+            return PartialView("_DataPartialView", data);
+        }
+    }
+}
---
+++ Models/Data.cs
@@ -0,0 +1,8 @@
+﻿namespace ModularAjaxPractice.Models
+{
+    public class Data
+    {
+        public int Id { get; set; }
+        public string Context { get; set; }
+    }
+}
